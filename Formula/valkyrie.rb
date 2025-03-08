e22fcd214f6e9f1397a9bd62f765f182aca985ec
Yap <egorikftp@gmail.com>
2025-03-08 10:34:00 +0300

    Bump valkyrie 0.13.0

--- Formula/valkyrie.rb
+++ Formula/valkyrie.rb
@@ -1,9 +1,9 @@
 class Valkyrie < Formula
   desc "A CLI tool to convert SVG/XML into Compose ImageVector"
   homepage "https://github.com/ComposeGears/Valkyrie"
-  url "https://github.com/ComposeGears/Valkyrie/releases/download/0.12.0/valkyrie-cli-0.12.0.zip"
-  version "0.12.0"
-  sha256 "07b68128233c505a6701bde03414f7a30809f8598a855bab4a6d56e7866daca7"
+  url "https://github.com/ComposeGears/Valkyrie/releases/download/0.13.0/valkyrie-cli-0.13.0.zip"
+  version "0.13.0"
+  sha256 "6fff370d69c93949b86eaa4b16df4c7f16ceb032d149f6b4fbc817adf12cac8e"
 
   depends_on "openjdk"
 
